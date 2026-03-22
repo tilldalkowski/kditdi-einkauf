3efb36a7636fe8e9bc8d2d074bf1ce8e347faa19
tilldalkowski <till@dalkowski.eu>
2026-03-22 14:33:48 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,37 @@
+'use client'
+
+import { useState } from 'react'
+import { supabase } from '../lib/supabaseClient'
+
 export default function Home() {
-  return <h1>Dalkowski Einkaufszettel</h1>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+  }
+
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Dalkowski Einkaufszettel</h1>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Passwort"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={login}>Login</button>
+    </main>
+  )
 }
